da3e78404f0da012d7afb01922165848334e364c
juanpfonseca08 <jpf13@sfu.ca>
2026-01-16 13:55:44 -0800

    lab attempt

--- lab1/lab1.c
+++ lab1/lab1.c
@@ -1,8 +1,26 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
 
-int main(void){
-  
+int main(void) {
+
+  char *line = NULL;
+  size_t n = 0;
+
+  printf("Please enter some text: \n");
+
+  if (getline(&line, &n, stdin) != -1) {
+    printf("Got: %s\n", line);
+    printf("Tokens:\n");
+    char *ret = NULL;
+    char *saveptr = NULL;
+    char *str = line;
+    while ((ret = strtok_r(str, " ", &saveptr))) {
+      printf("%s\n", ret);
+      str = NULL;
+    }
+  } else {
+    printf("Failure reading line!\n");
+  }
   return 0;
 }
